1ad30e497146bbd17ab8194719e3ba4f758eac4e
Shiraj Sayed <shirajsayed13@gmail.com>
2022-02-07 12:17:27 +0530

    Refactor code

--- app/src/main/java/com/shiraj/goodwalltask/image_with_rx/data/BitmapWithQuality.kt
+++
@@ -1,8 +0,0 @@
-package com.shiraj.goodwalltask.image_with_rx.data
-
-import android.graphics.Bitmap
-
-data class BitmapWithQuality(
-    val bitmap: Bitmap,
-    val quality: Int
-)
--- app/src/main/java/com/shiraj/goodwalltask/image_with_rx/data/ResponseState.kt
+++
@@ -1,5 +0,0 @@
-package com.shiraj.goodwalltask.image_with_rx.data
-
-enum class ResponseState {
-    LOADING, ERROR, SUCCESS
-}
